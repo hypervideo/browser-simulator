4456cf227727f15939671f021003ca03a09ea3fb
Robert Krahn <robert.krahn@gmail.com>
2025-10-24 00:00:40 +0200

    add deps

--- flake.nix
+++ flake.nix
@@ -8,30 +8,33 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in
-        {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              rustc
-              cargo
-              clippy
-              pkg-config
-            ];
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            clippy
+            pkg-config
+          ];
 
-            buildInputs = with pkgs; [
-              openssl
-              clang
-            ] ++ (if pkgs.stdenv.isDarwin then [ libiconv ] else [ ]);
+          buildInputs = with pkgs; [
+            openssl
+            clang
+            ffmpeg-headless
+          ] ++ (if pkgs.stdenv.isDarwin then [ libiconv ] else [ ]);
 
-            packages = with pkgs; [
-              rust-analyzer
-              (rustfmt.override { asNightly = true; })
-              cargo-nextest
-            ];
+          packages = with pkgs; [
+            rust-analyzer
+            (rustfmt.override { asNightly = true; })
+            cargo-nextest
+          ]
+          ++ lib.optionals pkgs.stdenv.isDarwin [ google-chrome ]
+          ++ lib.optionals (!pkgs.stdenv.isDarwin) [ chromium ];
 
-            RUST_BACKTRACE = "1";
-            RUST_LOG = "debug";
-            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-          };
-        }
+          RUST_BACKTRACE = "1";
+          RUST_LOG = "debug";
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        };
+      }
     );
 }
